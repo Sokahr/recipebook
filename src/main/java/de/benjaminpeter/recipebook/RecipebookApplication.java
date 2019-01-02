a945261265082c812bac601116d645027fd66978
Benjamin Peter <ben61279@gmail.com>
2019-01-02 06:57:12 +0100

    Initial commit

---
+++ src/main/java/de/benjaminpeter/recipebook/RecipebookApplication.java
@@ -0,0 +1,14 @@
+package de.benjaminpeter.recipebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipebookApplication.class, args);
+	}
+
+}
+
